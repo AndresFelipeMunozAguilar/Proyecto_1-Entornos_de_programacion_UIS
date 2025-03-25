77b6f747d0c9f2956007c7b7ddb600799ddbd3e6
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-03-25 18:10:22 -0500

    crear interfaz de employee

---
+++ demo/src/main/java/com/example/demo/InterfaceServices/IEmployee.java
@@ -0,0 +1,10 @@
+package com.example.demo.InterfaceServices;
+
+import java.util.List;
+import com.example.demo.Models.Employee;
+
+public interface IEmployee {
+
+    List<Employee> getEmployees();
+
+}
