e6b6a0f5dd57a8555a02868790e3a039a01b4966
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-03-25 17:40:10 -0500

    crear la clase job para manejar las filas de la tabla job

---
+++ demo/src/main/java/com/example/demo/Models/Job.java
@@ -0,0 +1,59 @@
+package com.example.demo.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = Job.TABLE_NAME)
+public class Job {
+
+    public static final String TABLE_NAME = "Job";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_job;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public Job() {
+        // Constructor vacío
+    }
+
+    public Job(int id_job, String name, String description) {
+        this.id_job = id_job;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getIdJob() {
+        return id_job;
+    }
+
+    public void setIdJob(int id_job) {
+        this.id_job = id_job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
