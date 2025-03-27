87254fd901619425586f710e69a59dace8a92755
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-03-26 20:22:02 -0500

    añadir casos donde hayan errores al guardar un empleado

--- demo/src/main/java/com/example/demo/Controllers/EmployeeController.java
+++ demo/src/main/java/com/example/demo/Controllers/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.example.demo.Controllers;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,11 +39,23 @@ public class EmployeeController {
     }
 
     @PostMapping("/createEmployee")
-    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+    public ResponseEntity<?> createEmployee(@RequestBody Employee employee) {
 
-        Employee savedEmployee = employeeService.createEmployee(employee);
+        try {
+            Employee savedEmployee = employeeService.createEmployee(employee);
+            return ResponseEntity.ok(savedEmployee);
 
-        return ResponseEntity.ok(savedEmployee);
+        } catch (NoSuchElementException e) {
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Error: El Job con id " + employee.getJob().getIdJob() + " no existe.");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Error: Datos inválidos en la solicitud.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error interno del servidor.");
+        }
 
     }
 
