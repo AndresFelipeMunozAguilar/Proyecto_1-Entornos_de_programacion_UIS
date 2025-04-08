59bd0de159f2935a412fcabedbf61af94a27a87b
Sergio Baron <sbaronrivera@gmail.com>
2025-04-08 16:38:38 -0500

    CRUD para tabla job

--- demo/src/main/java/com/example/demo/repository/JobRepository.java
+++ demo/src/main/java/com/example/demo/repository/JobRepository.java
@@ -2,7 +2,17 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface JobRepository extends JpaRepository<Job, Integer> {
+
+    boolean existsByName(String name);
+
+    boolean existsByIdJob(Integer idJob);
+
+    @Query("SELECT a.name FROM Job a")
+    List<String> findAllJobs();
 
 }
