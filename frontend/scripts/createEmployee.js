6ec55d26d9dd1aba8eba137e9d4b5ef63da7d3c9
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-04-10 17:18:52 -0500

    implementar un dropdown button para seleccionar el trabajo que le corresponde al empleado

--- frontend/scripts/createEmployee.js
+++ frontend/scripts/createEmployee.js
@@ -5,6 +5,7 @@ document.getElementById('create-employee-form').addEventListener('submit', funct
         name: document.getElementById('name').value,
         lastName: document.getElementById('lastName').value,
         phone: document.getElementById('phone').value,
+        jobId: document.getElementById('form-selected-job').value,
         imageUrl: document.getElementById('imageUrl').value,
     };
 
@@ -14,4 +15,28 @@ document.getElementById('create-employee-form').addEventListener('submit', funct
     // axios.post('http://localhost:8080/api/employee/addEmployee', employee)
     //   .then(response => console.log("Empleado creado:", response.data))
     //   .catch(error => console.error("Error al crear empleado:", error));
+});
+
+
+// Función para cambiar el contenido del dropdown button, 
+// basado en la elección del usuario
+// Escucha los clics en los ítems del dropdown
+document.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const selectedValue = this.getAttribute('data-value');
+        const selectedValueText = this.textContent.trim();
+        const button = document.getElementById('dropdownMenuButton1');
+        /**
+         * Recupera el elemento de entrada oculto utilizado para almacenar 
+         * la información del trabajo seleccionado en el formulario. Este 
+         * elemento es crucial para actualizar dinámicamente el texto del 
+         * botón de modo que refleje el nombre trabajo seleccionado.
+         */
+        const hiddenInput = document.getElementById('form-selected-job');
+
+        button.textContent = `Cargo seleccionado: ${selectedValueText}`;
+        hiddenInput.value = selectedValue;
+    });
 });
